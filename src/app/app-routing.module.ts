e1417abbfcfdf8ab80c23d3ebfd2960c473c9ed3
Alex Hill <alexhilldev@gmail.com>
2019-11-06 16:56:01 -0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkOrdersComponent } from './components/work-orders/work-orders.component'
+import { NotFoundComponent } from './components/not-found/not-found.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'work-orders', component: WorkOrdersComponent },
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkOrderComponent } from './components/work-order/work-order.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { WorkOrdersComponent } from './components/work-orders/work-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkOrderComponent,
+    NotFoundComponent,
+    WorkOrdersComponent
   ],
   imports: [
     BrowserModule,
